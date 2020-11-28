1b49edbf334b9b2127f4d3cd557510b1f873556e
singhratan97 <74782504+singhratan97@users.noreply.github.com>
2020-11-28 13:59:36 +0100

    Return median of two sorted arrays of same size

--- median.js
+++ median.js
@@ -1,19 +1,24 @@
-((arr1,arr2) => {
-arr = [...arr1,...arr2];
-arr.sort((a,b) => a-b);
-console.log(arr);
-
-//Median of two sorted array of same size when its EVEN
-if(arr.length%2 === 0)
-{
-    mid1 = (arr.length/2)-1;
-    mid2 = arr.length/2;
-    console.log((arr[mid1]+arr[mid2])/2);
-}
-//Median of two sorted array of same size when its ODD
-else
-{
-    mid = parseInt(arr.length/2);
-    console.log(arr[mid]);
-}
-})([1,2,3],[8,7,6]);
+    ((arr1,arr2) => {
+    
+    arr1.sort((a,b) => a-b);
+    arr2.sort((a,b) => a-b);
+    
+    arr = [...arr1,...arr2];
+        
+    console.log(arr);
+    
+    //Median of two sorted array of same size when its EVEN
+    if(arr.length%2 === 0)
+    {
+        mid1 = (arr.length/2)-1;
+        mid2 = arr.length/2;
+        console.log((arr[mid1]+arr[mid2])/2);
+    }
+    //Median of two sorted array of same size when its ODD
+    else
+    {
+        mid = parseInt(arr.length/2);
+        console.log(arr[mid]);
+    }
+    })([8,4,2],[8,9,1]);
+    
